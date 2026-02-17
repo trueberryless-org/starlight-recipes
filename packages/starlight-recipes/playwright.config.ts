97fac6c5840007f08f36ea5c5d3db686d3ff0fa6
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-02-17 17:33:26 +0100

    fix: higher timeout

--- packages/starlight-recipes/playwright.config.ts
+++ packages/starlight-recipes/playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "tests/e2e",
   testMatch: "**/*.test.ts",
+  timeout: 60000,
   fullyParallel: true,
   forbidOnly: !!process.env["CI"],
   projects: [
